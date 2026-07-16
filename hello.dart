af4e04e6a8b5196ab026fa6964f4a59cb27237c5
ThonaDev <thonaben08@gmail.com>
2026-07-16 23:55:33 +0700

    add other input using stdout.write

--- hello.dart
+++ hello.dart
@@ -5,4 +5,9 @@ void main(){
     stdout.write("Enter your name: ");
     String name = stdin.readLineSync()!;
     print("Hello, $name!");
+
+    stdout.write("Enter your age: ");
+    int age = int.parse(stdin.readLineSync()!);
+    print("You are $age years old.");
+
 }
