fd079e8be261087a2bd7c1b5176a73fc475df802
ThonaDev <thonaben08@gmail.com>
2026-07-20 18:35:42 +0700

    about var type

---
+++ variable2.dart
@@ -0,0 +1,10 @@
+void main(){
+  var name = "Ben";
+  var age = 21;
+  var isStudent = true;
+  isStudent = false;
+
+  // can not reassign it to a different type of value
+  // isStudent = "Yes"; // This will give an error
+  print(isStudent);
+}
