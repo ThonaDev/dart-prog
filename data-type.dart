1411fb47ba5e6b8eb6af6a239a2e27c7f1a019c6
ThonaDev <thonaben08@gmail.com>
2026-07-20 19:18:49 +0700

    data type num and string interpolation

---
+++ data-type.dart
@@ -0,0 +1,23 @@
+void main(){
+  // num is the superclass of int and double. 
+  //It can hold both integer and floating-point values.
+
+  num a = 10;
+  num b = 10.5;
+
+  print("The value of a is: $a");
+  print("The value of b is: $b");
+
+  String name = "Dudu";
+  String name1 = 'Bubu';
+
+  // both single and double quotes can be used to declare a string variable.
+  print("The value of name is: $name");
+  print("The value of name1 is: $name1");
+
+  // String interpolation
+  // instead of using + 
+  print("My name is: " + name);
+  // we use $ to access the value of a variable inside a string.
+  print("My name is: $name");
+}
