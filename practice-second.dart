73b63d596720ae22eafface9367f79f7c56da836
ThonaDev <thonaben08@gmail.com>
2026-07-20 18:45:48 +0700

    practice exercie 2

---
+++ practice-second.dart
@@ -0,0 +1,10 @@
+void main(){
+  String name = "Dudu";
+  int age = 22;
+  String favProg = "C++";
+  bool isStudent = true;
+
+  print("My name is $name and I am $age years old.");
+  print("My favorite programming language is $favProg.");
+  print("Am I a student? $isStudent");
+}
