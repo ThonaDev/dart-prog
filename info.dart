af4f64ab758e0c9bced815482799996a92c397a7
ThonaDev <thonaben08@gmail.com>
2026-07-20 09:05:30 +0700

    add some info about myself

---
+++ info.dart
@@ -0,0 +1,10 @@
+void main(){
+
+  // This is a simple Dart program that prints some info about me.
+  print("My name is Thona.");
+  print("I am learning Dart.");
+  print("I want to become a Flutter developer.");
+  /*
+    here is multi-line comments.
+  */
+}
