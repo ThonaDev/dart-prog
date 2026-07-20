b8630a49c9e3ce664e39bdc4846b8018b0c06a61
ThonaDev <thonaben08@gmail.com>
2026-07-20 19:55:15 +0700

    conversion exercise 2

--- practice-fourth.dart
+++ practice-fourth.dart
@@ -10,4 +10,15 @@ into an integer, then adds 50.*/
   print("The value of num is: $num");
   print("The type after conversion is: ${num.runtimeType}");
   print("The value after adding 50 is: $addedValue");
+
+
+ /*  Write a program that converts: "3.5"
+  into a double, multiplies it by 2, and prints the result.
+  Expected output: 7.0*/
+
+  String text2 = "3.5";
+  double num2 = double.parse(text2);
+  double multiValue = num2 * 2;
+
+  print("The result after multiValue is: $multiValue");
 }
