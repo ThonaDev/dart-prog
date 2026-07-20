f20f79de6bd76d9e2ef5606d7b08865ee50b2074
ThonaDev <thonaben08@gmail.com>
2026-07-20 09:10:22 +0700

    this is exercise 1

---
+++ practice-first.dart
@@ -0,0 +1,8 @@
+void main(){
+  print("=========================================");
+  print("Student Information");
+  print("=========================================");
+  print("Name : Thona");
+  print("Age : 21");
+  print("Country : Cambodia");
+}
