33395d10e40c248712a9cd0f4e13fcdacbac9572
ThonaDev <thonaben08@gmail.com>
2026-07-20 19:45:18 +0700

    practice 3

---
+++ practice-third.dart
@@ -0,0 +1,21 @@
+void main(){
+  /*===== Profile =====
+  Name : Ben
+  Age : 21
+  Height : 1.72
+  Weight : 60.5
+  Student : true*/
+
+  String name = "Dudu";
+  int age = 22;
+  double height = 1.65;
+  double weight = 57;
+  bool isStudent = true;
+
+  print("====== Prfile =======");
+  print("Name: $name");
+  print("Age: $age");
+  print("Height: $height");
+  print("Weight: $weight");
+  print("Student: $isStudent");
+}
