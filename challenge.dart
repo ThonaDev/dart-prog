27aa5c2dc5a2c4cd23f60bf0f64d79b12a7e61ad
ThonaDev <thonaben08@gmail.com>
2026-07-20 18:53:10 +0700

    challenge exercise

---
+++ challenge.dart
@@ -0,0 +1,26 @@
+/*======== Profile ========
+Name     : Ben
+Age      : 21
+Country  : Cambodia
+Height   : 1.72
+Student  : true
+=========================*/
+
+
+void main(){
+  // Profile
+
+  String name = "Thona";
+  int age = 22;
+  String country = "Cambodia";
+  double height = 1.65;
+  bool isStudent = true;
+
+  print("========================");
+  print("Name: $name");
+  print("Age: $age");
+  print("Country: $country");
+  print("Height: $height");
+  print("Student: $isStudent");
+
+}
