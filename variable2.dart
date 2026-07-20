4c219afec2401c95817d46eef414eae3d03d03e6
ThonaDev <thonaben08@gmail.com>
2026-07-20 18:40:35 +0700

    The differ between final vs const

--- variable2.dart
+++ variable2.dart
@@ -1,4 +1,5 @@
 void main(){
+  // using var to declare a variable
   var name = "Ben";
   var age = 21;
   var isStudent = true;
@@ -7,4 +8,16 @@ void main(){
   // can not reassign it to a different type of value
   // isStudent = "Yes"; // This will give an error
   print(isStudent);
+
+
+  // final vs const
+  // final variable can be assigned only once and it can be assigned at runtime.
+  final String country = "Cambodia";
+  // country = "USA"; // This will give an error
+
+  // const variable can be assigned only once and it must be assigned at compile time.
+  const double pi = 3.14;
+
+  print("I from $country");
+  print("The value of pi is: $pi");
 }
