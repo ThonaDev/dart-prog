91e265d3846c13cb9abf3f362ca7796f94c2fe1d
ThonaDev <thonaben08@gmail.com>
2026-07-21 23:46:02 +0700

    incre loop by 2

--- loop.dart
+++ loop.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 void main(){
   // for loop
   for(int i=1; i<=100; i++){
@@ -8,6 +10,11 @@ void main(){
   print("---------- backward loop -----------");
   // counting backward
   for(int i = 10; i>0; i--){
+    print(i);
+  }
+
+  print("----- incre by 2 ------");
+  for(int i=1; i<=10; i+=2){
     print(i);
   }
 }
