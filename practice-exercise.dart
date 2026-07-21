e9be78c534345c8235503adeae9fe6a586a07db4
ThonaDev <thonaben08@gmail.com>
2026-07-21 18:25:55 +0700

    find negative, positive or zero

---
+++ practice-exercise.dart
@@ -0,0 +1,23 @@
+void main(){
+  /*
+  Practice 1 — Positive or Negative
+  Create:
+  int number = -5;
+  Check if the number is:
+  Positive
+  Negative
+  Zero
+*/
+
+  int number = 0;
+  if(number > 0){
+    print("Number $number is positive number.");
+  }else if(number < 0){
+    print("Number $number is negative number.");
+  }else{
+    print("Number $number is zero.");
+  }
+
+  
+
+}
