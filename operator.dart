f466bcfeb9f0a4dbad244ce71d32e7d2b64e1cc0
ThonaDev <thonaben08@gmail.com>
2026-07-21 13:38:44 +0700

    add operator in dart

---
+++ operator.dart
@@ -0,0 +1,18 @@
+void main(){
+  int a = 5;
+  int b = 10; 
+
+  print(a + b);
+  print("${a + b}"); // if we need add some text.
+
+  print(a - b);
+  print(a * b);
+  print(a / b);
+
+  int x = 10;
+  int y = 5;
+
+  print(x / y);
+  print(x ~/ y);
+
+}
