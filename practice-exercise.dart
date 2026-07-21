1c901ea4aa9899d92cdfaaed4864b942600b2fb2
ThonaDev <thonaben08@gmail.com>
2026-07-21 18:28:48 +0700

    find even or odd number

--- practice-exercise.dart
+++ practice-exercise.dart
@@ -18,6 +18,19 @@ void main(){
     print("Number $number is zero.");
   }
 
-  
+
+  /*
+  Practice 2 — Even or Odd
+  Create:
+  int number = 10;
+  Check whether the number is even or odd.
+*/
+
+  int num = 3;
+  if(num % 2 == 0){
+    print("Number $num is even number.");
+  }else{
+    print("Number $num is odd number.");
+  }
 
 }
