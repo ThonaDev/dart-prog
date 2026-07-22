6b36d599f52feeebd05380fd69cef0eb5743027d
ThonaDev <thonaben08@gmail.com>
2026-07-22 18:36:20 +0700

    function that return value

--- function.dart
+++ function.dart
@@ -10,9 +10,27 @@ void sayGreeting(){
 }
 
 
+// function with multi-para
+void introSelf(String name, int age){
+  print("My name is $name");
+  print("I am $age years old.");
+}
+
+
+// function that return value
+  int addValue(int a, int b){
+    return a + b;
+  }
+
 void main(){
+  // call function
   sayHello("Dudu");
   sayHello("Thona");
   sayHello("Rambo");
   sayGreeting();
+
+  introSelf("Thona", 22);
+  
+  int result = addValue(5, 5);
+  print(result);
 }
