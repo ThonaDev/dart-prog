f4dc6338d419c1dde01a41597cdc2f75f1aaab70
ThonaDev <thonaben08@gmail.com>
2026-07-22 18:25:03 +0700

    function without para

--- function.dart
+++ function.dart
@@ -1,7 +1,18 @@
+
+// functino with parameter
 void sayHello(String text){
   print("Hello, $text !");
 }
 
+// function without parameter
+void sayGreeting(){
+  print("Greeting !!");
+}
+
+
 void main(){
   sayHello("Dudu");
+  sayHello("Thona");
+  sayHello("Rambo");
+  sayGreeting();
 }
