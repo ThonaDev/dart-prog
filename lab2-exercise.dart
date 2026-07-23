11733d1fa8f4f293fe826ae6e4d96fce1cfa18a1
ThonaDev <thonaben08@gmail.com>
2026-07-23 08:13:37 +0700

    practice exercise

---
+++ lab2-exercise.dart
@@ -0,0 +1,16 @@
+void main(){
+  // given a textual data representing for expenses:
+  /*(111, 300)/(215, 600)/(28, 1900)...
+  Dart code to practice expenses information as following:
+  No      Amount
+  --------------
+  111     300.00
+  215     600.00
+  28     1900.00
+  --------------
+  Total Amount: $ ...
+  */
+
+
+
+}
