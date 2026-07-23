51838ff9f5bdfed6fc86805e8a4c427d9cb08078
ThonaDev <thonaben08@gmail.com>
2026-07-23 09:22:38 +0700

    done the exercise lab2

--- lab2-exercise.dart
+++ lab2-exercise.dart
@@ -11,6 +11,42 @@ void main(){
   Total Amount: $ ...
   */
 
+  String header = "${"No".padRight(8)} ${"Amount".padLeft(10)}";
+  String bar = "-" * 19;
+  String info = "";
+  String totalInfo = "";
 
+  String data = "(111,300)/(215,600)/(281,1900)";
+  var expTexts = data.split("/");
 
+  double total = 0;
+  info = "";
+
+  for(var text in expTexts){
+    if(!(text.contains("(") && text.contains(")"))){
+      continue;
+    }
+
+    var temp = text.replaceAll("(", "").replaceAll(")", "");
+    var items = temp.split(",");
+    var noText = items[0].trim();
+    var amount = double.parse(items[1]);
+
+    total += amount;
+
+    if(info != ""){
+      info += "\n";
+    }
+
+    info += "${noText.padRight(8)} ${amount.toStringAsFixed(2).padLeft(10)}";
+
+  }
+
+  totalInfo = "Total Amount: \$ ${total.toStringAsFixed(2)}";
+
+  print(header);
+  print(bar);
+  print(info);
+  print(bar);
+  print(totalInfo);
 }
